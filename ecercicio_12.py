86b4f51309148094f7bb194579fc1d136d74f3a4
rafael <rcm2005@icloud.com>
2023-03-27 17:10:47 -0300

    adicionando comentarios no exercicio 12

--- ecercicio_12.py
+++ ecercicio_12.py
@@ -1,15 +1,14 @@
 comprimento = float(input("Digite o comprimento da cozinha em metros: "))
-altura = float(input("digite a altura da cozinha em metros: "))
-largura = float(input("digite a largura da cozinha em metros: "))
+altura = float(input("digite a altura da cozinha em metros: "))     #solicitando as medidas das paredes
+largura = float(input("digite a largura da cozinha em metros: "))  # obs: as medidas devem ser inseridas em metros
 
-paredesmenores = largura * altura * 2
-paredesmaiores = comprimento * altura *2
+paredesmenores = largura * altura * 2     #somando a area das duas paredes menores iguais
+paredesmaiores = comprimento * altura *2    #somando area paredes maiores
 
-caixa = float(1.5)
+caixa = float(1.5)         # determinando a area em asulejos por caixa
 
+areatotal = paredesmaiores + paredesmenores #calculando a area total somando as paredes
 
-areatotal = paredesmaiores + paredesmenores 
-
-qntcaixas = areatotal / caixa
+qntcaixas = areatotal / caixa   #descobrindo a quantidade total de caixas que serão necessárias
 
 print(qntcaixas)
